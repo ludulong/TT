c29ff239168ec30e3d38bf8afa91d8f2db0e6444
ludulong <376643318@qq.com>
2023-07-20 11:49:13 +0800

    hot44  commit

--- src/main/java/GGGOOO/GIT/GOGO.java
+++ src/main/java/GGGOOO/GIT/GOGO.java
@@ -14,5 +14,11 @@ public class GOGO {
         System.out.println("hot gogo3333");
         System.out.println("hot gogo88888888888888");
         System.out.println("hot gogo3333");
+        System.out.println("hot gogo3333");
+        System.out.println("hot gog4444444444444444");
+
+
+
+
     }
 }
