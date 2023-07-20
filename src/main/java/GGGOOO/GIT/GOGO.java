2ddbfeef57feb61536eb4c2a533391fa755f1214
ludulong <376643318@qq.com>
2023-07-20 10:54:44 +0800

    third commit

--- src/main/java/GGGOOO/GIT/GOGO.java
+++ src/main/java/GGGOOO/GIT/GOGO.java
@@ -4,5 +4,8 @@ public class GOGO {
     public static void main(String[] args) {
         System.out.println("hello gogo");
         System.out.println("hello gogo22");
+        System.out.println("hello gogo3333");
+
+
     }
 }
