f5c17e8b59b221df69aee42a36d2847a22f29f25
ludulong <376643318@qq.com>
2023-07-20 10:53:47 +0800

    second commit

--- src/main/java/GGGOOO/GIT/GOGO.java
+++ src/main/java/GGGOOO/GIT/GOGO.java
@@ -3,5 +3,6 @@ package GGGOOO.GIT;
 public class GOGO {
     public static void main(String[] args) {
         System.out.println("hello gogo");
+        System.out.println("hello gogo22");
     }
 }
